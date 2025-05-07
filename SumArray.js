1b990201c77be7e28e13d12edb751499b7c8cfd7
Ayushpaney24 <ayushpandey4480@gmail.com>
2025-05-07 13:24:41 +0530

    7th

---
+++ SumArray.js
@@ -0,0 +1,8 @@
+// Write a function that takes an array of numbers and returns the sum of all elements.✔️
+function sumArrray() {
+  let arr = [10, 654, 9, 4];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) sum += arr[i];
+  return sum;
+}
+console.log(sumArrray());
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function sumArrray() {
+  let arr = [10, 654, 9, 4];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) sum += arr[i];
+  return sum;
+}
+console.log(sumArrray());
